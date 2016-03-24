5bd4d1fceada62fb40fa375ab5b4d2bff24ae1ea
clampork <kairen@gmail.com>
2016-03-24 02:34:05 -0700

    Change drag threshold

--- javascripts/elastiStack.js
+++ javascripts/elastiStack.js
@@ -49,7 +49,7 @@
 
 	ElastiStack.prototype.options = {
 		// distDragBack: if the user stops dragging the image in a area that does not exceed [distDragBack]px for either x or y then the image goes back to the stack 
-		distDragBack : 200,
+		distDragBack : 50,
 		// distDragMax: if the user drags the image in a area that exceeds [distDragMax]px for either x or y then the image moves away from the stack 
 		distDragMax : 450,
 		// callback
