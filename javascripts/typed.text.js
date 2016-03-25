aacc3a8f07adf9d88cf82d330974cbb70e06b5ce
clampork <kairen@gmail.com>
2016-03-24 18:07:19 -0700

    Optimize for mobile

--- javascripts/typed.text.js
+++ javascripts/typed.text.js
@@ -1,11 +1,11 @@
 $(function(){
 	$(".typed").typed({
 		strings: [
-			"I do a bunch of data science work at Envoy.", 
+			"I do a bunch of data science work over at Envoy.", 
 			"On good days, I write miniaturist short fiction.",
-			"When it comes to food, I'm a committed hedonist.",
-			"These days, I play way less poker than I'd like.", 
-			"It's fairly obvious that my spirit animal is clam."
+			"My death row meal would be an Umami burger. And a Coke.",
+			"Nowadays, I play way less poker than I'd like.", 
+			"I believe my spirit animal to be clam.",
 		],
 		typeSpeed: 20,
 		backSpeed: 1,
