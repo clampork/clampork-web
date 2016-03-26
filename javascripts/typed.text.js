e49efcbb6077973934d0d57e8f4cd1c9ee4f1e40
clampork <kairen@gmail.com>
2016-03-26 16:12:16 -0700

    Change typed text

--- javascripts/typed.text.js
+++ javascripts/typed.text.js
@@ -4,8 +4,8 @@ $(function(){
 			"I do data science stuff over at Envoy.", 
 			"Every once in a while, I write short fiction.",
 			"6-7 suited is my favorite hand in poker.",
-			"My death row meal would be Umami burger and Mexican Coke.",
-			"If I had a spirit animal, it would be a clam."
+			"My death row meal would be: Umami burger, Mexican Coke, fries.",
+			"If I had a spirit animal, it would be clam."
 		],
 		typeSpeed: 20,
 		backSpeed: 1,
