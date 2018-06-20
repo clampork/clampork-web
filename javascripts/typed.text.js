3298b3147954492b21f99d3e1bed8e016caf33c8
Kai Chan <clampork@fb.com>
2018-06-20 11:38:21 -0700

    Change company text

--- javascripts/typed.text.js
+++ javascripts/typed.text.js
@@ -1,7 +1,7 @@
 $(function(){
 	$(".typed").typed({
 		strings: [
-			"I do data science stuff over at Envoy.", 
+			"I do data science stuff over at Facebook.",
 			"Every once in a while, I write short fiction.",
 			"6-7 suited is my favorite hand in poker.",
 			"My death row meal would be: Umami burger, Mexican Coke, fries.",
